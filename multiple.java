d04b083e222ea9a465ac556bd18737ce4b804c3a
manojmahi90526 <23hr1a3012@mtieat.org>
2024-12-04 16:03:52 +0530

    Add files via upload

---
+++ multiple.java
@@ -0,0 +1,17 @@
+import java.util.*;
+class multipleof4
+{
+    public static void main(String args[])
+    {
+        int a=15;
+        if((a&3)==0)
+        {
+        System.out.println("multiple of 4");
+        }
+        else
+        {
+        System.out.println("not multiple of 4");
+        }
+
+    }
+}
